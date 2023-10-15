1173497056d52009c0f35b98ec44e3a34e00944f
Nicolás Bugliot <nicobugliot@gmail.com>
2023-10-15 00:23:21 -0300

    add login and registation front without backend

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Router, Routes} from 'react-router-dom';
+import LoginForm from './login/LoginForm';
+import RegistrationForm from './registration/RegistrationForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/registro" element={<RegistrationForm/>} />
+      <Route path="/" element={<LoginForm/>} />
+    </Routes>
   );
 }
 
