e9d5f9c9a4041dcb29c8686dca7639f913afc5f0
Nicolás Bugliot <nicobugliot@gmail.com>
2023-10-22 18:22:15 -0300

    registration working with navigate

--- app/src/registration/RegistrationForm.js
+++ app/src/registration/RegistrationForm.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Container, TextField, Button, Grid, Typography } from '@mui/material';
-import { AccountCircle, Email, Lock } from '@mui/icons-material';
+import { Email, Lock } from '@mui/icons-material';
 import axios from "axios";
 import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 
 function RegistrationForm() {
+    const navigate = useNavigate();
+
     const [formData, setFormData] = useState({
         email: '',
         password: '',
@@ -25,16 +28,23 @@ function RegistrationForm() {
 
         console.log("Posting data", formData)
 
+        //TODO: Loading modal set true
+
         axios
             .post(apiUrl, formData)
             .then((response) => {
                 console.log('Registration:', response.data);
                 Cookies.set('id', response.data['id'], { expires: 1 });
+                //TODO: Loading modal set false
+                navigate('/');
             })
             .catch((error) => {
                 console.error('Error trying to register:', error);
+                //TODO: Loading modal set false
+                //TODO: Error modal
             });
     };
+
 
     return (
         <Container maxWidth="sm">
