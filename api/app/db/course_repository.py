0a21606f1b8b71b31925936734383f554500d4e7
Matías Ignacio González <maigonzalez@fi.uba.ar>
2023-11-01 15:37:20 -0300

    Crear curso

--- api/app/db/course_repository.py
+++ api/app/db/course_repository.py
@@ -16,15 +16,15 @@ class CourseRepository:
         raw_courses = self.session.exec(statement)
         return [course.to_read_model() for course in raw_courses]
 
-    def create(self, course: CourseCreate) -> CourseRead:
-        course = Course.from_create_model(course, self)
+    def create(self, course: CourseCreate, user_id: int) -> CourseRead:
+        course = Course.from_create_model(course, user_id)
         self.session.add(course)
         self.session.commit()
 
         return course.to_read_model()
 
-    def get_by_name(self, name: str) -> Optional[CourseRead]:
-        statement = select(Course).where(Course.name == name)
+    def get_by_title(self, title: str) -> Optional[CourseRead]:
+        statement = select(Course).where(Course.title == title)
         course = self.session.exec(statement).first()
         if course:
             return course.to_read_model()
