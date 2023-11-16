31fee185af54177b46fcaeb37d1d0620ad7b3d9f
Facundo Bravo <fbravo@dlocal.com>
2023-11-15 22:55:47 -0300

    Adds search bar

--- app/src/components/CourseList/CourseList.js
+++ app/src/components/CourseList/CourseList.js
@@ -1,18 +1,8 @@
+import React from "react";
 import "./CourseList.css";
-import {Component} from "react";
+import { Component } from "react";
 import axios from "axios";
-import {
-  Box, Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Grid, Rating,
-  Tab,
-  Tabs,
-  Typography
-} from "@mui/material";
+import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Rating, Tab, Tabs, Typography, TextField } from "@mui/material";
 
 class CourseCard extends Component {
   render() {
@@ -50,49 +40,66 @@ class CourseCard extends Component {
 }
 
 
-class CourseList extends Component {
+class CourseList extends React.Component {
   state = {
     courses: [],
     category: 0,
+    searchQuery: ''
   }
 
-  constructor(props) {
-    super(props);
-
-    axios.get('http://localhost:8000/courses').then((response) => {
-      this.setState({courses: response.data});
-    }).catch((error) => {
-      console.error('Error fetching courses: ', error);
-    });
+  componentDidMount() {
+    axios.get('http://localhost:8000/courses')
+      .then((response) => {
+        this.setState({ courses: response.data });
+      })
+      .catch((error) => {
+        console.error('Error fetching courses: ', error);
+      });
   }
 
   handleTabChange = (event, newValue) => {
-    this.setState({category: newValue});
+    this.setState({ category: newValue });
+  }
+
+  handleSearchChange = (event) => {
+    this.setState({ searchQuery: event.target.value.toLowerCase() });
   }
 
   render() {
-    const {courses, category} = this.state;
+    const { courses, category, searchQuery } = this.state;
+    const filteredCourses = courses.filter(course =>
+      (category === 5 ? true : course.category === category) &&
+      course.title.toLowerCase().includes(searchQuery)
+    );
+
     return (
       <Box className={"CourseList"}>
         <Typography variant={"h3"} className={"CourseList-title"}>Explora cursos</Typography>
-        <Box sx={{borderBottom: 1, borderColor: 'divider', mb: 3}}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', borderBottom: 1, borderColor: 'divider', mb: 3 }}>
           <Tabs value={category} onChange={this.handleTabChange} textColor="black" indicatorColor="secondary">
-            <Tab label="Programación" className={category === 0 ? 'CourseList-tab-selected' : 'CourseList-tab'}/>
-            <Tab label="Matemática" className={category === 1 ? 'CourseList-tab-selected' : 'CourseList-tab'}/>
-            <Tab label="Marketing" className={category === 2 ? 'CourseList-tab-selected' : 'CourseList-tab'}/>
-            <Tab label="Economía" className={category === 3 ? 'CourseList-tab-selected' : 'CourseList-tab'}/>
-            <Tab label="Arte" className={category === 4 ? 'CourseList-tab-selected' : 'CourseList-tab'}/>
+            <Tab label="Programación" />
+            <Tab label="Matemática" />
+            <Tab label="Marketing" />
+            <Tab label="Economía" />
+            <Tab label="Arte" />
+            <Tab label="Todos" />
           </Tabs>
+          <TextField
+            variant="outlined"
+            placeholder="Buscar por título"
+            onChange={this.handleSearchChange}
+            size="small"
+          />
         </Box>
         <Grid container spacing={5}>
-        {courses.filter((course) => (course.category === category)).map((course) => (
-          <Grid item xs={12} sm={6} md={4} key={course.id}>
-            <CourseCard course={course}/>
-          </Grid>
-        ))}
+          {filteredCourses.map(course => (
+            <Grid item xs={12} sm={6} md={4} key={course.id}>
+              <CourseCard course={course} />
+            </Grid>
+          ))}
         </Grid>
       </Box>
-    )
+    );
   }
 }
 
