092ffbe07d29f88d48bbdabefed1b1b4a1bc7162
Facundo Bravo <fbravo@dlocal.com>
2023-11-15 23:12:09 -0300

    Acomoda mas parecido al figma

--- app/src/components/CourseList/CourseList.js
+++ app/src/components/CourseList/CourseList.js
@@ -66,41 +66,42 @@ class CourseList extends React.Component {
   }
 
   render() {
-    const { courses, category, searchQuery } = this.state;
-    const filteredCourses = courses.filter(course =>
-      (category === 5 ? true : course.category === category) &&
-      course.title.toLowerCase().includes(searchQuery)
-    );
+  const { courses, category, searchQuery } = this.state;
+  const filteredCourses = courses.filter(course =>
+    (category === 5 ? true : course.category === category) &&
+    course.title.toLowerCase().includes(searchQuery)
+  );
 
-    return (
-      <Box className={"CourseList"}>
-        <Typography variant={"h3"} className={"CourseList-title"}>Explora cursos</Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', borderBottom: 1, borderColor: 'divider', mb: 3 }}>
-          <Tabs value={category} onChange={this.handleTabChange} textColor="black" indicatorColor="secondary">
-            <Tab label="Programación" />
-            <Tab label="Matemática" />
-            <Tab label="Marketing" />
-            <Tab label="Economía" />
-            <Tab label="Arte" />
-            <Tab label="Todos" />
-          </Tabs>
-          <TextField
-            variant="outlined"
-            placeholder="Buscar por título"
-            onChange={this.handleSearchChange}
-            size="small"
-          />
-        </Box>
-        <Grid container spacing={5}>
-          {filteredCourses.map(course => (
-            <Grid item xs={12} sm={6} md={4} key={course.id}>
-              <CourseCard course={course} />
-            </Grid>
-          ))}
-        </Grid>
+  return (
+    <Box className={"CourseList"}>
+      <Typography variant={"h3"} className={"CourseList-title"}>Explora cursos</Typography>
+      <TextField
+        variant="outlined"
+        placeholder="Buscar por título"
+        onChange={this.handleSearchChange}
+        size="small"
+        className={"CourseList-search"}
+      />
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3 }}>
+        <Tabs value={category} onChange={this.handleTabChange} textColor="black" indicatorColor="secondary">
+          <Tab label="Programación" />
+          <Tab label="Matemática" />
+          <Tab label="Marketing" />
+          <Tab label="Economía" />
+          <Tab label="Arte" />
+          <Tab label="Todos" />
+        </Tabs>
       </Box>
-    );
-  }
+      <Grid container spacing={5}>
+        {filteredCourses.map(course => (
+          <Grid item xs={12} sm={6} md={4} key={course.id}>
+            <CourseCard course={course} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
 }
 
 export default CourseList;
