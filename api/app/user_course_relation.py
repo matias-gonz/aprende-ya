d3695fffd17ba4a9e2c23c29ccd14c4ef0073a63
Filyan Karagoz <fily@maplabs.com>
2023-11-20 23:59:28 -0300

    adds profile fixes

--- api/app/user_course_relation.py
+++ api/app/user_course_relation.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class UserCourseRelationBase(BaseModel):
     user_id: int = Field(description="User id")
     course_id: int = Field(description="Course id")
     is_finished: bool = Field(description="If user finished course")
-    review: str = Field(description="User review")
-    rating: int = Field(description="User rating stars (from 0 to 5)")
+    review: Optional[str] = Field(description="User review", default=None)
+    rating: Optional[int] = Field(description="User rating stars (from 0 to 5)", default=None)
 
 
 class UserCourseRelationCreate(UserCourseRelationBase):
