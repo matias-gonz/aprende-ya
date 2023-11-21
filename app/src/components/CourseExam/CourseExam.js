1bd7850d0a281c6444bc58260d8054a7f0a3ebff
matias-gonz <maigonzalez@fi.uba.ar>
2023-11-21 00:10:25 -0300

    Add Exam submit

--- app/src/components/CourseExam/CourseExam.js
+++ app/src/components/CourseExam/CourseExam.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-import {Button, Container, FormControl, FormControlLabel, Radio, RadioGroup, Typography} from "@mui/material";
+import {Button, Container, FormControl, FormControlLabel, Modal, Radio, RadioGroup, Typography} from "@mui/material";
+import Cookies from "js-cookie";
+import {useParams} from "react-router-dom";
+import axios from "axios";
 
-const CourseExam = ({ questions }) => {
+const CourseExam = ({ questions, course_id }) => {
+    const [score, setScore] = useState(0);
+    const [certificado, setCertificado] = useState("");
+    const [showModal, setShowModal] = useState(false);
     const parsedQuestions = JSON.parse(questions);
     const [selectedAnswers, setSelectedAnswers] = useState({});
 
     const handleSelectAnswer = (questionIndex, optionIndex) => {
+        console.log(questionIndex, optionIndex);
         setSelectedAnswers({
             ...selectedAnswers,
             [questionIndex]: optionIndex,
@@ -13,14 +20,30 @@ const CourseExam = ({ questions }) => {
     };
 
     const handleCompleteExam = () => {
-        let score = 0;
+        let s = 0;
         parsedQuestions.forEach((question, index) => {
             if (selectedAnswers[index] === question.options.indexOf(question.answer)) {
-                score++;
+                s++;
             }
         });
-        alert(`Tu puntaje es: ${score}/${parsedQuestions.length}`);
-        // Aquí podrías hacer otras acciones con el puntaje, como mostrarlo en el modal, enviarlo a una base de datos, etc.
+        setScore(s);
+        setShowModal(true);
+
+        const user_id = Cookies.get('user_id');
+
+        axios.post(`http://127.0.0.1:8000/certificates/${user_id}?course_id=${course_id}`, {withCredentials: true})
+          .then(response => {
+              console.log('Certificate generated:', response.data);
+              setCertificado(response.data['hash']);
+          })
+          .catch(error => {
+              console.error('Error generating certificate:', error);
+          });
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        window.location.reload();
     };
 
     return (
@@ -31,7 +54,7 @@ const CourseExam = ({ questions }) => {
                     <FormControl component="fieldset">
                         <RadioGroup
                             aria-label={`question${questionIndex}`}
-                            value={selectedAnswers[questionIndex] || ''}
+                            value={selectedAnswers[questionIndex] | ''}
                             onChange={(event) => handleSelectAnswer(questionIndex, parseInt(event.target.value))}
                         >
                             {question.options.map((option, optionIndex) => (
@@ -49,6 +72,19 @@ const CourseExam = ({ questions }) => {
             <Button variant="contained" color="primary" onClick={handleCompleteExam}>
                 Completar Examen
             </Button>
+            <Modal open={showModal} onClose={handleCloseModal}>
+                <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: '#fff', padding: '20px', border: '1px solid #ccc', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
+                    <Typography variant={"h3"} style={{fontSize: '3rem', fontWeight: 'bold'}}>¡Felicidades!</Typography>
+                    <br />
+                    <Typography variant={"h3"}>Tu puntuacion es: {score}/{parsedQuestions.length}</Typography>
+                    <br />
+                    <Typography variant={"h3"}>Tu certificado es:</Typography>
+                    <p>{certificado}</p>
+                    <br />
+                    <br />
+                    <Button onClick={handleCloseModal}>Close</Button>
+                </div>
+            </Modal>
         </Container>
     );
 };
